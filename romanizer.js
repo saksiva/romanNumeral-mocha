41c399c51338f98db097d96efc2efa3ed057d27f
saksiva <41472850+saksiva@users.noreply.github.com>
2019-04-18 21:58:21 +0100

    Update romanizer.js

--- romanizer.js
+++ romanizer.js
@@ -1,5 +1,34 @@
+const values = [
+  { arabic: 1000, roman: "M" },
+  { arabic: 900, roman: "CM" },
+  { arabic: 500, roman: "D" },
+  { arabic: 400, roman: "CD" },
+  { arabic: 100, roman: "C" },
+  { arabic: 90, roman: "XC" },
+  { arabic: 50, roman: "L" },
+  { arabic: 40, roman: "XL" },
+  { arabic: 10, roman: "X" },
+  { arabic: 9, roman: "IX" },
+  { arabic: 5, roman: "V" },
+  { arabic: 4, roman: "IV" },
+  { arabic: 1, roman: "I" }
+];
+
 const romanizer = num => {
-  return "I";
+  var remaining = num;
+  var result = "";
+
+  if (num >= 5000) return "Sorry, the Roman could not count this high!";
+
+  values.forEach(value => {
+    while (remaining >= value.arabic) {
+      result += value.roman;
+
+      remaining -= value.arabic;
+    }
+  });
+  return result;
 };
 
 module.exports = romanizer;
+
